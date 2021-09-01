a10522dc003484d8f7e7b92324762cf3bc736c7d
Eric Jumba <eric.jumba@yellowpageskenya.com>
2021-09-01 12:59:18 +0300

    ffds

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Spatie\Sitemap\SitemapGenerator;
 use Illuminate\Support\Facades\Storage;
+use Artesaos\SEOTools\Facades\SEOTools;
 
 class MainController extends Controller
 {
     //
     public function index()
     {
+        SEOTools::setTitle('Create your Free Google Sitemap Online - Google XML Sitemaps Generator');
+        SEOTools::setDescription('Free Online Unlimited Google XML Sitemap Generator. Free online sitemap generator service, creating an XML sitemap that can be submitted to Google, Bing and other search engines to help them crawl your website better.');
+        SEOTools::opengraph()->setUrl('http://current.url.com');
+        SEOTools::setCanonical('https://codecasts.com.br/lesson');
+        SEOTools::opengraph()->addProperty('type', 'articles');
+        SEOTools::twitter()->setSite('@LuizVinicius73');
+        SEOTools::jsonLd()->addImage('https://codecasts.com.br/img/logo.jpg');
+
         return view('home');
     }
 
